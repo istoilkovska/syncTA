17f36f80767893cf45cbb2a7cf0662b1b2cf5259
istoilkovska <anili100@gmail.com>
2020-07-20 15:29:14 +0200

    added faircons rcvta

--- README.md
+++ README.md
@@ -4,32 +4,30 @@ Encodings of synchronous fault-tolerant distributed algorithms as synchronous th
 
 We verify safety of these algorithms using bounded model checking, that is, by encoding safety violations as reachability queries on bounded executions. Using two SMT-based procedures, we first find the bound on the length of the executions we consider, and then check reachability of a bad state.
 
-<h4>Prerequisites</h4>
-We currently support the SMT solvers <a href="https://github.com/Z3Prover/z3">Z3</a> and <a href="https://github.com/CVC4/CVC4">CVC4</a>.
-To be able to run our scripts, make sure you have Python, Z3 and CVC4 installed, and on your $PATH. 
+### Prerequisites
+We currently support the SMT solvers [Z3](https://github.com/Z3Prover/z3) and [CVC4](https://github.com/CVC4/CVC4).
+To be able to run our scripts, make sure you have Python, Z3 and CVC4 installed, and on your `$PATH`. 
 
-<h3>Contents</h3>
+### Contents
 There are three packages:
-<ul>
-  <li><code>algorithms</code>, containing the algorithms for which we compute the diameter using SMT</li>
-  <li><code>boundable</code>, containing the algorithms for which additionally we have a theoretical bound on the diameter</li>
-  <li><code>conuterexamples</code>, containing erroneous encodings of the algorithms</li>
-</ul>  
+- `algorithms`, containing the algorithms for which we compute the diameter using SMT
+- `boundable`, containing the algorithms for which additionally we have a theoretical bound on the diameter
+- `conuterexamples`, containing erroneous encodings of the algorithms
 
-<h3>Running the experiments</h3>
+## Running the experiments
 You can check the safety of the encoded algorithms by 
-<code>
+```bash
 $ python run.py solver
-</code>
+```
 
 You can check the safety of the algorithms for which we have a theoretical bound on the diameter by
-<code>
+```bash
 $ python run_boundable.py solver
-</code>
+```
 
 You can check that the erroneous versions contain counterexamples by 
-<code>
+```bash
 $ python run_counterexamples.py solver
-</code>
+```
 
-In all cases, <code>solver</code> is either <code>z3</code> or <code>cvc4</code>.
+In all cases, `solver` is either `z3` or `cvc4`.
---
+++ receiveSTA/rcv_faircons.py
@@ -0,0 +1,61 @@
+# process local states
+local = range(11)
+# L states
+L = {"x0" : [0, 2, 3], "x1" : [1, 4], "cr0" : [5, 7, 8], "cr1" : [6, 9], "v0" : [0], "v1" : [1], "d0" : [2, 3], "d1" : [4]}
+# initial states
+initial = [0, 1, 5, 6]
+
+# rules
+rules = []
+rules.append({'idx': 0, 'from': 0, 'to': 2, 'guard': "true"})
+rules.append({'idx': 1, 'from': 1, 'to': 4, 'guard': "(< nr0 1)"})
+rules.append({'idx': 2, 'from': 1, 'to': 3, 'guard': "(>= nr0 1)"})
+rules.append({'idx': 3, 'from': 2, 'to': 2, 'guard': "true"})
+rules.append({'idx': 4, 'from': 4, 'to': 4, 'guard': "(< nr0 1)"})
+rules.append({'idx': 5, 'from': 4, 'to': 3, 'guard': "(>= nr0 1)"})
+rules.append({'idx': 6, 'from': 3, 'to': 2, 'guard': "true"})
+rules.append({'idx': 7, 'from': 0, 'to': 7, 'guard': "true"})
+rules.append({'idx': 8, 'from': 1, 'to': 9, 'guard': "(< nr0 1)"})
+rules.append({'idx': 9, 'from': 1, 'to': 8, 'guard': "(>= nr0 1)"})
+rules.append({'idx': 10, 'from': 2, 'to': 7, 'guard': "true"})
+rules.append({'idx': 11, 'from': 4, 'to': 9, 'guard': "(< nr0 1)"})
+rules.append({'idx': 12, 'from': 4, 'to': 8, 'guard': "(>= nr0 1)"})
+rules.append({'idx': 13, 'from': 3, 'to': 7, 'guard': "true"})
+rules.append({'idx': 14, 'from': 5, 'to': 10, 'guard': "true"})
+rules.append({'idx': 15, 'from': 6, 'to': 10, 'guard': "true"})
+rules.append({'idx': 16, 'from': 7, 'to': 10, 'guard': "true"})
+rules.append({'idx': 17, 'from': 8, 'to': 10, 'guard': "true"})
+rules.append({'idx': 18, 'from': 9, 'to': 10, 'guard': "true"})
+rules.append({'idx': 19, 'from': 10, 'to': 10, 'guard': "true"})
+
+# parameters, resilience condition
+params = ["n", "t", "f"]
+active = "n"
+rc = ["(> n 0)", "(>= t 0)", "(>= t f)", "(> n t)"]
+
+# faults
+faults = "crash"
+faulty = [5, 6, 7, 8, 9, 10]
+crashed = [3, 4, 5]
+max_faulty = "f"
+phase = 1
+
+# configuration/transition constraints
+constraints = []
+constraints.append({'type': 'configuration', 'sum': 'eq', 'object': local, 'result': active})
+constraints.append({'type': 'configuration', 'sum': 'le', 'object': faulty, 'result': max_faulty})
+constraints.append({'type': 'transition', 'sum': 'eq', 'object': range(len(rules)), 'result': active})
+constraints.append({'type': 'round_config', 'sum': 'eq', 'object': crashed, 'result': 0})
+
+# receive environment constraints
+environment = []
+environment.append('(>= nr0 x0)')
+environment.append('(<= nr0 (+ x0 cr0))')
+environment.append('(>= nr1 x1)')
+environment.append('(<= nr1 (+ x1 cr1))')
+
+# properties
+properties = []
+properties.append({'name':'validity0', 'spec':'safety', 'initial':'(= x0 n)', 'qf':'last', 'reachable':'(> d1 0)'})
+properties.append({'name':'validity1', 'spec':'safety', 'initial':'(= x1 n)', 'qf':'last', 'reachable':'(> d0 0)'})
+properties.append({'name':'agreement', 'spec':'safety', 'initial':'true', 'qf':'last', 'reachable':'(and (> d0 0) (> d1 0))'})
